e490d02cbed1a46b191911207d088786f95b8421
Ollie Lee <myself.ollie.lee@gmail.com>
2022-07-27 13:24:33 +0800

    feat: add pm2 for deploy

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-require('express-async-errors');
-const v1Router = require('./routes');
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+require("express-async-errors");
+const v1Router = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.use('/v1', v1Router);
+app.use("/v1", v1Router);
 
 module.exports = app;
